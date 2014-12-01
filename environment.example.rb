8347cfc76e277f04376e2aa10e01ffe4d64c65bf
Jonathan Hooper <Jonathan@newaperio.com>
2014-12-01 13:37:09 -0600

    Fixing some bugs with getting started
    - Renamed Gemfile so bundler can find it
    - Fix some variable names that got changed in environment.rb, but not environment.example.rb

--- environment.example.rb
+++ environment.example.rb
@@ -1,10 +1,10 @@
 # Mailer configs
-ENV['KITTEN_SMTP_SERVER_FROM_EMAIL'] = 'your_emailh@gmail.com'
-ENV['KITTEN_SMTP_SERVER_ADDRESS'] = 'smtp.gmail.com'
-ENV['KITTEN_SMTP_SERVER_PORT'] = '587'
-ENV['KITTEN_SMTP_SERVER_DOMAIN'] = 'gmail.com'
-ENV['KITTEN_SMTP_SERVER_USERNAME'] = 'your_email@gmail.com'
-ENV['KITTEN_SMTP_SERVER_PASSWORD'] = 'your_password'
+ENV['KITTEN_SMTP_FROM_EMAIL'] = 'your_emailh@gmail.com'
+ENV['KITTEN_SMTP_ADDRESS'] = 'smtp.gmail.com'
+ENV['KITTEN_SMTP_PORT'] = '587'
+ENV['KITTEN_SMTP_DOMAIN'] = 'gmail.com'
+ENV['KITTEN_SMTP_USERNAME'] = 'your_email@gmail.com'
+ENV['KITTEN_SMTP_PASSWORD'] = 'your_password'
 
 # Loader configs
 ENV['KITTEN_LOADER_SAVE_PATH'] = "/tmp/kitten"
