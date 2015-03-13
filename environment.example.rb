5d750b124b60340498e7124a2af8d1c2ad110433
Jonathan Hooper <Jonathan@newaperio.com>
2015-03-12 21:47:21 -0500

    Removing GEMFILE

--- environment.example.rb
+++ environment.example.rb
@@ -5,7 +5,7 @@ ENV['KITTEN_SMTP_PORT'] = '587'
 ENV['KITTEN_SMTP_DOMAIN'] = 'gmail.com'
 ENV['KITTEN_SMTP_USERNAME'] = 'your_email@gmail.com'
 ENV['KITTEN_SMTP_PASSWORD'] = 'your_password'
-
+ 
 # Loader configs
 ENV['KITTEN_LOADER_SAVE_PATH'] = "/tmp/kitten"
 ENV['KITTEN_LOADER_SUBREDDIT'] = 'cats'
