6e5a912e504ceb59d19836bfb83264f6f289b927
Jonathan Hooper <jon9820@gmail.com>
2017-07-02 09:50:20 -0500

    Change login method in SMTP settings

--- kitten_dispatch/kitten_mailer.rb
+++ kitten_dispatch/kitten_mailer.rb
@@ -5,7 +5,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     address: ENV['KITTEN_SMTP_ADDRESS'],
     port: Integer(ENV['KITTEN_SMTP_PORT']),
-    authentication: :plain,
+    authentication: :login,
     domain: ENV['KITTEN_SMTP_DOMAIN'],
     user_name: ENV['KITTEN_SMTP_USERNAME'],
     password: ENV['KITTEN_SMTP_PASSWORD'],
@@ -13,22 +13,22 @@ ActionMailer::Base.smtp_settings = {
   }
 
 module KittenDispatch
-  
+
   class KittenMailer < ActionMailer::Base
-    
+
     def mail_kitten(hash)
       # Load values from the hash
       recipient = hash[:recipient]
       caption = hash[:caption]
       file_path = hash[:file_path]
-            
+
       # Prepare the email
       attachments.inline['photo.png'] = File.read(file_path)
       email = mail(to: recipient, from: ENV['KITTEN_SMTP_FROM_EMAIL'], subject: caption, body: caption)
-      
+
       # Send the email
       email.deliver
     end
-    
+
   end
 end
